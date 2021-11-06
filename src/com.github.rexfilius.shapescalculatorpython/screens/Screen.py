c9b5b958abe337d9d26294afa3b707a90f3d06be
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2021-11-06 22:09:42 +0100

    Create functions for screen menu in Screen.py

---
+++ src/com.github.rexfilius.shapescalculatorpython/screens/Screen.py
@@ -0,0 +1,51 @@
+INVALID_DATA = "\nYOU ENTERED INVALID DATA\n"
+INVALID_CASING = "\nCHECK YOUR CASING/WORDS. TYPE CORRECTLY\n"
+
+select = ""
+parameter = ""
+
+
+def first_menu() -> str:
+    firstMessage = ('This Application Calculates The Area And Perimeter Of 2-Dimensional Shapes'
+                    'List of Shapes: [Triangle, Circle, Square, Rectangle, Parallelogram, Trapezium]'
+                    'Select a Shape: [Type it in]')
+    print(firstMessage)
+    global select
+    select = str(input())
+    check_first_menu()
+    return select
+
+
+def check_first_menu():
+    while (select != 'Circle' or select != 'Triangle' or select != 'Square'
+           or select != 'Rectangle' or select != 'Parallelogram'
+           or select != 'Trapezium'):
+        print(INVALID_CASING)
+        first_menu()
+
+
+def second_menu() -> str:
+    print("What do you want to calculate: Area? or Perimeter? [Type it in]")
+    global parameter
+    parameter = str(input())
+    check_second_menu()
+    return parameter
+
+
+def check_second_menu():
+    while parameter != 'Area' or parameter != 'Perimeter':
+        print(INVALID_CASING)
+        second_menu()
+
+
+# TODO: Handle exception here should the user give a wrong input format
+def third_menu():
+    print("\nPress 1 to go back to MENU\nPress 2 to END")
+    thirdMenuInput = int(input())
+    if thirdMenuInput == 1:
+        print('')  # TODO: replace with run_calculator function from Calculator.py
+    elif thirdMenuInput == 2:
+        print('End of program\n')
+    else:
+        print('Invalid input')
+        third_menu()
